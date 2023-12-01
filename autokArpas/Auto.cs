aabf6f94035b9b30d1f55a2264d08e439a8a71fb
Kevin <arpaskevin11@gmail.com>
2023-12-01 08:01:53 +0100

    Add project files.

---
+++ autokArpas/Auto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autokArpas
+{
+    class Auto
+    {
+        public string GyartoEsModell { get; set; }
+        public string SAESzint { get; set; }
+        public double AktualisSebesseg { get; set; }
+        public string Szenzorok { get; set; }
+        public double KoordinataSzelesseg { get; set; }
+        public double KoordinataHosszusag { get; set; }
+        public int VezetoiBeavatkozasok { get; set; }
+        public string VezetesiMod { get; set; }
+
+        public static string Atalakito(string GyartoEsModell)
+        {
+            return GyartoEsModell.ToUpper();
+        }
+
+        public Auto(string sor)
+        {
+            var a = sor.Split("; ");
+            GyartoEsModell = a[0];
+            SAESzint = a[1];
+            AktualisSebesseg = double.Parse(a[2]);
+            Szenzorok = a[3];
+            var koordinatak = a[4].Split("|");
+            KoordinataSzelesseg = double.Parse(koordinatak[0]);
+            KoordinataHosszusag = double.Parse(koordinatak[1]);
+
+            VezetoiBeavatkozasok = int.Parse(a[5]);
+            VezetesiMod = a[6];
+        }
+
+        public override string ToString() => $"Gyártó és modell: {GyartoEsModell} {GyartoEsModell}; SAE-szint: {SAESzint}; Aktuális sebesség: {AktualisSebesseg} km/h; Szenzorok: {Szenzorok}; X-koordináta: {KoordinataSzelesseg}; Y-koordináta: {KoordinataHosszusag}; Vezetői beavatkozások száma: {VezetoiBeavatkozasok}, Vezetési mód: {VezetesiMod}";
+    }
+}
---
+++ autokArpas/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace autokArpas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var autok = new List<Auto>();
+
+            using var sr = new StreamReader(@"..\..\..\src\autok.txt", Encoding.UTF8);
+
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                autok.Add(new(sr.ReadLine()));
+            }
+
+            foreach (var a in autok)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.WriteLine("7. feladat");
+
+            var felautomatikus = _7f(autok);
+
+            if (felautomatikus.Count == 0)
+            {
+                Console.WriteLine("Nincs ilyen autó");
+            }
+            else
+            {
+                Console.WriteLine($"{felautomatikus.Count} ilyen autó van.");
+            }
+
+            Console.WriteLine("8. feladat");
+
+            var legkevesbeOnvezetok = _8f(autok);
+
+            Console.WriteLine("Legkevésbé önálló autók:");
+            if (legkevesbeOnvezetok.Count == 0)
+            {
+                Console.WriteLine("Hiba! Nincs ilyen autó.");
+            }
+            else
+            {
+                foreach (var l in legkevesbeOnvezetok)
+                {
+                    Console.WriteLine(l);
+                }
+                Console.WriteLine($"..azaz {legkevesbeOnvezetok.Count} ilyen db van.");
+                Console.WriteLine($"Max érték: {legkevesbeOnvezetok[0].VezetoiBeavatkozasok}");
+            }
+
+            Console.WriteLine("9. feladat");
+
+            Console.WriteLine($"A legkisebb szélességi koordináta: {_9f(autok)}");
+
+            Console.WriteLine("10. feladat");
+
+            var gyartok = _10f(autok);
+
+            if (gyartok.Count == 0)
+            {
+                Console.WriteLine("Nincs ilyen autó!");
+            }
+            else
+            {
+                Console.WriteLine("Gyártók:");
+                foreach (var g in gyartok)
+                {
+                    Console.WriteLine(g);
+                }
+            }
+
+            Console.WriteLine("11. feladat");
+
+            for (int i = 0; i < autok.Count; i++)
+            {
+                if (_11f(autok[i]))
+                {
+                    Console.Write($"{i+2} ");
+                }
+            }
+
+            Console.WriteLine("\n12. feladat");
+
+            using var sw = new StreamWriter(@"..\..\..\src\nagybetusek.txt", false, Encoding.UTF8);
+
+            foreach (var a in autok)
+            {
+                sw.WriteLine($"{Auto.Atalakito(a.GyartoEsModell)}, {a.SAESzint}");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static List<Auto> _7f(List<Auto> autok) => autok.Where(a => a.SAESzint == "félautomatikus" && a.VezetesiMod == "manuális").ToList();
+        public static List<Auto> _8f(List<Auto> autok) => autok.Where(a => a.VezetoiBeavatkozasok == autok.Max(a => a.VezetoiBeavatkozasok)).ToList();
+        public static double _9f(List<Auto> autok) => autok.Where(a => a.KoordinataSzelesseg == autok.Min(a => a.KoordinataSzelesseg)).Select(a => a.KoordinataSzelesseg).ToList()[0];
+        public static List<string> _10f(List<Auto> autok) => autok.Where(a => a.VezetesiMod == "automatikus").Select(a => a.GyartoEsModell.Split(" ")[0]).Distinct().OrderBy(a => a).ToList();
+        public static bool _11f(Auto auto) => auto.AktualisSebesseg > 85 && auto.AktualisSebesseg < 95 && auto.Szenzorok.Split(",").Length >= 3;
+    }
+}
